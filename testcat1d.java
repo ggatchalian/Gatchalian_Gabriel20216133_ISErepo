c4c6577efc5e094214d57d9a033ed0ca4165b4ae
ggatchalian <103607100+ggatchalian@users.noreply.github.com>
2022-05-30 18:46:47 +0800

    Update testcat1d.java

--- testcat1d.java
+++ testcat1d.java
@@ -12,7 +12,7 @@ class Main {
         System.out.println(Code.removeNumberLowercase("Hello World"));
         System.out.println(Code.removeNumberLowercase("6133"));
         System.out.println(Code.removeNumberLowercase("Gatchalian"));
-        System.out.println(Code.removeNumberLowercase("Gabriel Gatchalian"));
+        System.out.println(Code.removeNumberLowercase("Gabriel Gareth GATCHALIAN"));
         System.out.println(Code.removeNumberLowercase("Thor 4"));
     }
 
